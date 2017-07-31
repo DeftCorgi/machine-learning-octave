167018fdcc05c4fc9c1aafbbd45fc30b809ee13c
DeftCorgi <toanau123@gmail.com>
2017-07-31 19:53:42 +1000

    got rid of your code here

--- week4/ex3/predictOneVsAll.m
+++ week4/ex3/predictOneVsAll.m
@@ -26,26 +26,4 @@ h = sigmoid(z);
 % populate p, matrix of probabilities for each class
 [temp, p] = max(h,[],2);
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Complete the following code to make predictions using
-%               your learned logistic regression parameters (one-vs-all).
-%               You should set p to a vector of predictions (from 1 to
-%               num_labels).
-%
-% Hint: This code can be done all vectorized using the max function.
-%       In particular, the max function can also return the index of the 
-%       max element, for more information see 'help max'. If your examples 
-%       are in rows, then, you can use max(A, [], 2) to obtain the max 
-%       for each row.
-%       
-
-
-
-
-
-
-
-% =========================================================================
-
-
 end

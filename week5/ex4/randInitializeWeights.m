e29bc82d072650e904250ca7b9417c9fef8571d8
DeftCorgi <toanau123@gmail.com>
2017-08-04 11:22:55 +1000

    implemented

--- week5/ex4/randInitializeWeights.m
+++ week5/ex4/randInitializeWeights.m
@@ -11,22 +11,8 @@ function W = randInitializeWeights(L_in, L_out)
 
 % You need to return the following variables correctly 
 W = zeros(L_out, 1 + L_in);
+epsilon = 0.12;
+W = rand(size(W)) * 2 * epsilon - epsilon;
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Initialize W randomly so that we break the symmetry while
-%               training the neural network.
-%
-% Note: The first column of W corresponds to the parameters for the bias unit
-%
-
-
-
-
-
-
-
-
-
-% =========================================================================
 
 end
